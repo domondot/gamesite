c812f7428e609ca0759defc383e4e6ff14bb2684
domondot <martin.salapatev@gmail.com>
2013-07-17 17:22:38 +0300

    changed response to index.html

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ var express = require('express')
 var app = express()
 
 app.get('/', function(req, res){
-	     res.send('hello world');
+	var buffer = fs.readFileSync('index.html')
+	res.send(buffer.toString());
 });
 
 app.listen(8080)
